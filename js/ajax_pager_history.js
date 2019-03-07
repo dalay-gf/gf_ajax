b1f15192cde6996ed59eb727ef699a6c4986210f
dalay <dalay@drupalka.ru>
2019-03-07 12:57:01 +0300

    Перенос ajax_pager_history.js из темы в модуль.

---
+++ js/ajax_pager_history.js
@@ -0,0 +1,19 @@
+(function ($) {
+    Drupal.behaviors.gfAjaxPagerHistory = {
+        attach: function (context, settings) {
+
+            // Заносим в историю бр-ра переходы по
+            // ajax-ссылкам пейджера в каталоге.
+            // $(".jquery-once-2-processed ul.pager a, .jquery-once-3-processed ul.pager a", context).each(function() {
+            $("div[class*=' jquery-once-'] ul.pager a", context).each(function() {
+                var path = ($(this).attr('href').substr(0, 1) != '/') ?
+                    '/' + $(this).attr('href') : $(this).attr('href');
+                $(this).click(function() {
+                    window.history.pushState(null, null, path);
+                    return false;
+                });
+            });
+        }
+    };
+})(jQuery);
+
